e5ecbc920cbd5275207d47f1e0043296ba26d000
Vincent-HD <vincenthoudan@gmail.com>
2025-07-19 01:45:00 +0200

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import { cloudflare } from 'unenv'
-import nitroCloudflareBindings from 'nitro-cloudflare-dev'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
-    tanstackStart()
+    tanstackStart({ customViteReactPlugin: true }),
+    viteReact(),
   ],
 })
